883e2c3d8972fda49aa84060455d6bc867d743f8
Darkness <huangrenzhong@kuajinghelp.com>
2018-05-13 15:17:02 +0800

    personal to users

---
+++ db/migrate/20180513070821_add_personal_to_users.rb
@@ -0,0 +1,11 @@
+class AddPersonalToUsers < ActiveRecord::Migration[5.2]
+  def change
+      add_column :users, :sex, :string
+      add_column :users, :birthday, :date
+      add_column :users, :province, :string
+      add_column :users, :city, :string
+      add_column :users, :school, :string
+      add_column :users, :industry, :string
+      add_column :users, :summary, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_13_062129) do
+ActiveRecord::Schema.define(version: 2018_05_13_070821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,13 @@ ActiveRecord::Schema.define(version: 2018_05_13_062129) do
     t.string "badge"
     t.string "company"
     t.boolean "authentication", default: false
+    t.string "sex"
+    t.date "birthday"
+    t.string "province"
+    t.string "city"
+    t.string "school"
+    t.string "industry"
+    t.text "summary"
     t.index ["open_id"], name: "index_users_on_open_id"
     t.index ["phone"], name: "index_users_on_phone"
     t.index ["token"], name: "index_users_on_token"
