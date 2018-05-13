f217b75c857d6de3333fcfd20b07ef6ab95d0f0c
Darkness <huangrenzhong@kuajinghelp.com>
2018-05-13 15:27:09 +0800

    psersonal api update-show

--- app/api/api/v1/concerns/users.rb
+++ app/api/api/v1/concerns/users.rb
@@ -15,6 +15,31 @@ module API
             user = ::User.create!(create_body.to_h)
             wrap_meta(token: user.token)
           end
+
+          desc '完善个人信息 [PUT /users/perfect]'
+          params do
+            requires :sex, type: String
+            requires :birthday, type: String
+            requires :province, type: String
+            requires :city, type: String
+            requires :school, type: String
+            requires :industry, type: String
+            requires :summary, type: String
+          end
+          put '/users/perfect' do
+            current_user
+            create_body = declared params
+            current_user.update!(create_body.to_h)
+            wrap_meta(token: @current_user.token)
+          end
+
+          desc '个人信息 [get /users/show]'
+          get '/users/show' do
+            current_user
+            wrap_meta(
+              user: current_user
+            )
+          end
         end
       end
     end
