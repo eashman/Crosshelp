b047bc42c9f879d72827cb79b252c567bf1e7695
Darkness <huangrenzhong@kuajinghelp.com>
2018-05-13 17:00:16 +0800

    vip-api

--- app/api/api/v1/concerns/users.rb
+++ app/api/api/v1/concerns/users.rb
@@ -7,8 +7,8 @@ module API
         included do
           desc '登录 [POST /users/login]'
           params do
-            requires :phone, type: String, desc: '手机号码'
-            requires :open_id, type: String, desc: '微信标识'
+            optional :phone, type: String, desc: '手机号码'
+            optional :open_id, type: String, desc: '微信标识'
           end
           post '/users/login' do
             create_body = declared params
@@ -16,28 +16,42 @@ module API
             wrap_meta(token: user.token)
           end
 
+          desc 'vip用户申请 [PATCH /users/vip]'
+          params do
+            requires :job, type: Virtus::Attribute::Boolean, desc: '高管'
+          end
+          patch '/users/vip' do
+            @current_user = current_user
+            if @current_user.job
+              wrap_meta(msg: '该用户已经申请Vip成功')
+            else
+              @current_user.update!(job: true)
+              wrap_meta(msg: '申请Vip成功')
+            end
+          end
+
           desc '完善个人信息 [PUT /users/perfect]'
           params do
-            requires :sex, type: String
-            requires :birthday, type: String
-            requires :province, type: String
-            requires :city, type: String
-            requires :school, type: String
-            requires :industry, type: String
-            requires :summary, type: String
+            optional :sex, type: String, desc: '性别'
+            optional :birthday, type: String, desc: '出生日期'
+            optional :province, type: String, desc: '省'
+            optional :city, type: String, desc: '市'
+            optional :school, type: String, desc: '毕业院校'
+            optional :industry, type: String, desc: '行业'
+            optional :summary, type: String, desc: '个人简介'
           end
           put '/users/perfect' do
-            current_user
+            @current_user = current_user
             create_body = declared params
-            current_user.update!(create_body.to_h)
+            @current_user.update!(create_body.to_h)
             wrap_meta(token: @current_user.token)
           end
 
           desc '个人信息 [get /users/show]'
           get '/users/show' do
-            current_user
+            user = current_user
             wrap_meta(
-              user: current_user
+              user: user
             )
           end
         end
