9f1dda8e91905ea4fbd8380b224e5c7754fd8f1f
Darkness <huangrenzhong@kuajinghelp.com>
2018-05-14 12:02:14 +0800

    load aliyun oss api

--- app/api/api/v1/concerns/upload.rb
+++ app/api/api/v1/concerns/upload.rb
@@ -15,7 +15,20 @@ module API
             object_key = SecureRandom.hex(16)
             name = "#{object_key+file.filename}"
             result = Utils::Uploader.bucket.put_object(name, file: tmpfile,content_type: file.type)
-            wrap_meta(result: result)
+            if result
+             wrap_meta(name: name)
+           else
+             wrap_meta(msg: '上传异常')
+           end
+          end
+
+          desc '下载阿里云oss [GET /upload/load]'
+          params do
+            requires :name, type: String, desc: '文件名'
+          end
+          get '/upload/load' do
+            result = Utils::Uploader.bucket.object_url(params.name,true,3600)
+            wrap_meta(url: result)
           end
         end
       end
