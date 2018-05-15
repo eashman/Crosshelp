e38d574533ffd1dec70da2e7a6fa1d27dca7bccc
Darkness <huangrenzhong@kuajinghelp.com>
2018-05-15 22:29:48 +0800

    fix sms code

--- app/api/api/v1/concerns/sms.rb
+++ app/api/api/v1/concerns/sms.rb
@@ -15,8 +15,8 @@ module API
             $redis.set("_phoneCode#{phone}",code);
             $redis.expire("_phoneCode#{phone}",3600);
             p $redis.get("_phoneCode#{phone}")
-            content= "{签名}您的短信验证码是{验证码}，请在15分钟内输入使用。超时请重新申请。"
-            Utils::Sms.sendCode(phone, content, "86",nil,code)
+            content= "{跨境帮}您的短信验证码是#{code}，请在15分钟内输入使用。超时请重新申请。"
+            result = Utils::Sms.sendCode(phone, content, "86",nil,nil)
           end
         end
       end
